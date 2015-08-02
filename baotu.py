204e75982a71594b33d8cd704ec5816a3447905b
Chun Zhang <zhangchun88@gmail.com>
2015-08-02 14:34:18 -0400

    add baotu

---
+++ baotu.py
@@ -0,0 +1,21 @@
+from game import Game
+from action import SingleClickAction
+
+IMAGES = [
+    'login',
+    'login2',
+    'ok',
+    'use',
+    'close'
+]
+
+
+def main():
+    game = Game()
+    for image in IMAGES:
+        game.addAction(SingleClickAction(image))
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
