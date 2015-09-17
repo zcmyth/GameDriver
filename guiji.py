340c7085704f9d6ba5aa26e4c8383ef7b3aa8856
Chun Zhang <zhangchun88@gmail.com>
2015-09-17 17:36:33 -0400

    add guaji

---
+++ guiji.py
@@ -0,0 +1,39 @@
+from game import Game
+from action import SingleClickAction
+import time
+
+IMAGES = [
+    'login',
+    'login2',
+    'ok',
+    'close'
+]
+
+def baby(g):
+  if g.find('baby'):
+    print 'Baby'
+    print '\a'
+
+
+def defence_for_target(g):
+  if g.find('guijiang'):
+    print 'Find it!!!!!!!!'
+    print '\a'
+    g.clickImage('cancel')
+    time.sleep(5)
+    g.clickImage('defence')
+    time.sleep(5)
+    g.clickImage('defence')
+    time.sleep(5)
+
+
+def main():
+    game = Game()
+    for image in IMAGES:
+        game.addAction(SingleClickAction(image))
+    game.addAction(defence_for_target)
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
